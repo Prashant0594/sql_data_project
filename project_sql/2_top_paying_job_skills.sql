bb67760b057fd21a98c802894b260034372816c2
Prashant Parate <prashant0594@gmail.com>
2025-04-12 16:41:30 +0530

    update

--- project_sql/2_top_paying_job_skills.sql
+++ project_sql/2_top_paying_job_skills.sql
@@ -1,3 +1,11 @@
+/* Questions: What skills are required for top paying data analyst jobs? 
+   - use the top 10 highest paying data analyst jobs from the first query
+   - add the specific skills required for these roles
+   - why? it provides a detailed look at the skills that are in demand for high-paying data analyst positions, helping job seekers to 
+    understand what skills they need to develop to be competitive in the job market. */
+
+
+
 SELECT name as company_name, job_title, salary_year_avg
 FROM job_postings_fact
 left JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
@@ -7,3 +15,4 @@ WHERE
     salary_year_avg IS NOT NULL
 ORDER BY salary_year_avg DESC
 LIMIT 10;
+
