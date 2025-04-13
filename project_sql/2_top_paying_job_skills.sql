75a6658d61d8e0aad5c101d1ae0a1bbc2c9d3cbe
Prashant Parate <prashant0594@gmail.com>
2025-04-13 16:39:14 +0530

    update

--- project_sql/2_top_paying_job_skills.sql
+++ project_sql/2_top_paying_job_skills.sql
@@ -6,13 +6,34 @@
 
 
 
-SELECT name as company_name, job_title, salary_year_avg
+SELECT name as company_name, job_title, skills, salary_year_avg
 FROM job_postings_fact
 left JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
+left JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id 
+
 WHERE 
     job_title_short = 'Data Analyst' AND
     job_work_from_home = TRUE AND
     salary_year_avg IS NOT NULL
-ORDER BY salary_year_avg DESC
-LIMIT 10;
+ORDER BY salary_year_avg DESC;
 
+/* simplified version of the above query, using CTEs to make it more readable and maintainable */
+WITH top_paying_jobs AS (
+    SELECT job_id,name as company_name, job_title, salary_year_avg
+    FROM job_postings_fact
+    left JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
+    WHERE 
+        job_title_short = 'Data Analyst' AND
+        job_work_from_home = TRUE AND
+        salary_year_avg IS NOT NULL
+    ORDER BY salary_year_avg DESC
+    LIMIT 10
+)
+SELECT top_paying_jobs.*, skills
+FROM top_paying_jobs
+LEFT JOIN skills_job_dim ON top_paying_jobs.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+ORDER BY salary_year_avg DESC;
+
+/* one can export the data from above query and analyze using python or R to get the top 10 skills required for the top paying data analyst jobs */ 
