6f7c3124eb388f2a30a7fd491d749f1fb82d4300
Prashant Parate <prashant0594@gmail.com>
2025-04-14 12:02:19 +0530

    update

---
+++ project_sql/5_optimal_skills.sql
@@ -0,0 +1,8 @@
+/* Question: 
+What are the most optimal skills to learn? (aka what skills are in demand and pay well?)
+- identify skills in high demand and associated with high salaries for data analyst positions
+- concentrates on remote positions with specified salaries
+- why? it provides insights into the skills that are both in demand and associated with higher salaries,
+helping job seekers to understand which skills are most valuable in the job market.
+*/
+
